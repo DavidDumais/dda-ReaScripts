4c0f153c791113e28666125bd493c4837fd12761
David Dumais <david@daviddumaisaudio.com>
2024-05-20 16:21:47 -0400

    add copy lua

---
+++ Example/dda_CURATE - Copy.lua
@@ -0,0 +1,25 @@
+--[[ 
+Description: dda_CURATE
+Version: 1.0.0
+About:
+  dda_CURATE – Instant sound curation for Reaper.
+
+  dda_CURATE is a set of Reaper scripts to help you rate, organize, and categorize your sounds quickly and easily.
+  
+    -  Rate your sounds using a color-coding system to quickly determine which ones you prefer and organize them together
+    -  Easily move sounds to child tracks to keep similar sounds together
+    -  Add unique description metadata to individual sounds as well as project metadata to all sounds in your collection
+    -  Reorder your sounds based on different variables including item length or rating to keep your project neat and tidy
+    -  Printout all project sound information in one easy-to-read, easy-to-copy-paste window
+    -  And more!
+
+Author: David Dumais
+
+Links:
+  Store Page: https://daviddumaisaudio.gumroad.com/dda_CURATE
+  
+Provides:
+  [main] *.lua
+--]]
+
+reaper.ShowMessageBox("dda_CURATE is a set of Reaper scripts to help you rate, organize, and categorize your sounds quickly and easily. \n\ndda_CURATE, dda_RATING, dda_METADATA, dda_CATEGORIZATION" , "nvk_TRACK", 0)
